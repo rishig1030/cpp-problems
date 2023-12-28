dade0b6f687e0c821254d1342e7d3b2a691b5124
rishig1030 <guptarishi1030@gmail.com>
2023-12-28 16:32:24 +0530

    target sum of triplet

--- Array1.cpp
+++ Array1.cpp
@@ -1,17 +1,20 @@
-// Find the total number of pair in an array whose sum is equal to the given value x
+// Find the total number of triplet in an array whose sum is equal to the given value x
 #include<iostream>
 using namespace std;
 int main(){
-    int array[]={2,3,4,5,3,2,3,5,2,2};
+    int array[]={3,1,2,4,0,6};
     int TS=5;
     int count=0;
-    for(int i=0;i<9;i++){
-        for(int j=i+1;j<10;j++){
-            if(array[i]+array[j] == TS){
-                count++;
+    for(int i=0;i<4;i++){
+        for(int j=i+1;j<5;j++){
+            for(int k=j+1;k<6;k++){
+                if(array[i]+array[j]+array[k]==TS){
+                    count++;
+                }
             }
         }
     }
     cout<<count;
     return 0;
 }
+//for pair we only have to use two loop
