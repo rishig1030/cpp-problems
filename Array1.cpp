b6d95c80f32d1bf00c1455276314a88d638664da
rishig1030 <guptarishi1030@gmail.com>
2023-12-28 14:11:50 +0530

    target sum of pairs

--- Array1.cpp
+++ Array1.cpp
@@ -1,5 +1,17 @@
+// Find the total number of pair in an array whose sum is equal to the given value x
 #include<iostream>
 using namespace std;
 int main(){
-    
+    int array[]={2,3,4,5,3,2,3,5,2,2};
+    int TS=5;
+    int count=0;
+    for(int i=0;i<9;i++){
+        for(int j=i+1;j<10;j++){
+            if(array[i]+array[j] == TS){
+                count++;
+            }
+        }
+    }
+    cout<<count;
+    return 0;
 }
