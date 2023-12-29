9e3117942a2195d061096c260f5b93a06dd96792
rishig1030 <guptarishi1030@gmail.com>
2023-12-29 16:03:59 +0530

    2nd largest in an array,updated

--- array3.cpp
+++ array3.cpp
@@ -2,18 +2,23 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    vector <int> a;
-    a={3225,42,2,42,527,321};
+int secondlargest(vector <int> a){
     int max=INT16_MIN,max2=INT16_MIN;
     for(int i=0;i<a.size();i++){
         if(max<a[i] && max2<a[i]){
             max2=max;
             max=a[i];
         }
-        else if(max2<a[i] && max>a[i]){
+        else if(max2<a[i] && max>=a[i]){
             max2=a[i];
         }
     }
-    cout<<max2;
+    return max2;
+}
+int main(){
+    int y;
+    vector <int> a;
+    a={3225,42,2,42,527,321};
+    y=secondlargest(a);
+    cout<<y;
 }
