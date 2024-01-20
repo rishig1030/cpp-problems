4d23297cc0bb165986b3a937bb1de7848ffa4e8c
rishig1030 <guptarishi1030@gmail.com>
2024-01-20 11:17:24 +0530

    decode string

--- string/str5.cpp
+++ string/str5.cpp
@@ -2,18 +2,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 string decodedstr(string s1){
-    int n;
-    string s;
+    //traversing a string
+    string result="";
     for(int i=0;i<s1.size();i++){
-        if(s[i]==)
+        
+        if(s1[i]!=']'){
+            result.push_back(s1[i]);
+        }
+        else{
+            //extracting str from result
+            string str="";
+            while(!result.empty() && result.back()!='['){ //result.empty , check is string is empty or not
+                str.push_back(result.back()); //back add last character
+                result.pop_back();// use to remove last character
+            }
+            //rervese an str
+            reverse(str.begin(),str.end());
+
+            //remove [ from str
+            result.pop_back();
+
+            //now extracting from string 
+            string num="";
+            while(!result.empty() && (result.back()>='0' && result.back()<='9')){
+                num.push_back(result.back());
+                result.pop_back();
+            }
+            
+            //reversing thee num string
+            reverse(num.begin(),num.end());
+
+            //convert string to number
+            int n=stoi(num);
+
+            //inserting str to result n times
+            while(n--){
+                result+=str;
+            }
+        }
     }
-    return s;
+    return result;
 }
 int main(){
     string s1;
     cin>>s1;
     
-    decodedstr(s1);
+    cout<<decodedstr(s1);
 
 
     return 0;
