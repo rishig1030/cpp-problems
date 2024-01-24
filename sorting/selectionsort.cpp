13700dd87eb909df10bb42afbf69edf1cf815c3c
rishig1030 <guptarishi1030@gmail.com>
2024-01-24 08:00:29 +0530

    selection sort

--- sorting/selectionsort.cpp
+++ sorting/selectionsort.cpp
@@ -1,2 +1,30 @@
 //selection sort
-#include<bit/
+#include<bits/stdc++.h>
+using namespace std;
+
+//repeatedly find minimun element and place it at beginning
+//stable sort :> element having same value are not disturbed
+//hence selection sort is not stable sorting algorithm
+int main(){
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    //time complexity would be worst case/best case O(n(n-1)/2) i.e. O(n)
+    for(int i=0;i<n-1;i++){
+        int m=i;
+        for(int j=i+1;j<n;j++){
+            if(v[j]<v[m]){
+                m=j;
+            }
+        }
+        if(v[m]!=v[i]){
+            swap(v[m],v[i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
