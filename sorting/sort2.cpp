b8067983ae86e994ed99aab250ca0e9a0b7be0d8
rishig1030 <guptarishi1030@gmail.com>
2024-01-29 09:12:48 +0530

    some modification in my approach

--- sorting/sort2.cpp
+++ sorting/sort2.cpp
@@ -2,11 +2,16 @@
 //selection sort - find minimum element and place it at the beginning 
 #include<bits/stdc++.h>
 using namespace std;
+//here the time complexity will be more than that of selection sort , i.e O(n^2 * m) ,m is the size of string 
+//but here i don't have to use any character array which i will in second approach (no problem although) 
 bool checksize(string &a,string &b){
     
     for(int i=0;i<min(a.size(),b.size());i++){
         if(int(a[i])<int(b[i])){
             return true;
+        }
+        else if(int(a[i])>int(b[i])){
+            return false;
         }
     }
     
@@ -15,7 +20,8 @@ void stringsort(vector<string> &v){
     for(int i=0;i<v.size()-1;i++){
         int m=i;
         for(int j=i+1;j<v.size();j++){
-            if(checksize(v[j],v[m])){
+            bool flag=checksize(v[j],v[m]);
+            if(flag){
                 m=j;
             }
         } 
