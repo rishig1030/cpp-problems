78203b19da5c9c24e37766d8826afbea113b2dda
rishig1030 <guptarishi1030@gmail.com>
2024-01-30 08:48:40 +0530

    recursion2.cpp to find nth fibonacci number

--- recursion/recursion2.cpp
+++ recursion/recursion2.cpp
@@ -1,8 +1,16 @@
-//fibonacci series using recursion 
+//use recursion to find the nth fibonacci number strign from 0. 
 #include<bits/stdc++.h>
-uisng namespace std;
-
+using namespace std;
+int fibonacci(int n){
+    if(n==1 || n==0){
+        return n;
+    }
+    return fibonacci(n-1)+fibonacci(n-2);
+}
 int main(){
-
+    int n;
+    cin>>n;
+    int y=fibonacci(n);
+    cout<<y;
     return 0;
 }
