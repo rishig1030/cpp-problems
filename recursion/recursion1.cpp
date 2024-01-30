4cee912b0f0befaa048852e44b2eaf0a8ea30b8d
rishig1030 <guptarishi1030@gmail.com>
2024-01-30 08:39:32 +0530

    factoriaL USING recursion

---
+++ recursion/recursion1.cpp
@@ -0,0 +1,15 @@
+//find the factorial using recursion
+#include<bits/stdc++.h>
+using namespace std;
+int factorial(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    else return n*factorial(n-1);
+}
+int main(){
+    int n;cin>>n;
+    int f=factorial(n);
+    cout<<f;
+    return 0;
+}
---
+++ recursion/recursion2.cpp
@@ -0,0 +1,8 @@
+//fibonacci series using recursion 
+#include<bits/stdc++.h>
+uisng namespace std;
+
+int main(){
+
+    return 0;
+}
