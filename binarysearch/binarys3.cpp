4802108bf517a2d3b5919a73691f548007f8dd59
rishig1030 <guptarishi1030@gmail.com>
2024-03-23 23:43:46 +0530

    binary search 3, lower bound

---
+++ binarysearch/binarys3.cpp
@@ -0,0 +1,27 @@
+//lower bound problem
+//write an minimum index which has number greater than equal to target value
+// by recursion method 
+//for iterative method use while(left<=high) {  and so on  }7
+#include<iostream>
+using namespace std;
+int f(int arr[],int left,int right,int target,int n){
+    if(left>right){
+        return n;
+    }
+    int mid=(left+right)/2;
+    if(arr[mid]>=target){
+        n=mid;
+        return f(arr,left,mid-1,target,n);   //this may be my answer but need smaller index
+    }
+    else{
+        return f(arr,mid+1,right,target,n);  //this means lesser , hence move to right side of array
+    }
+}
+int main(){
+    int arr[]={3,4,12,19,40,40,54,54};
+    int target;
+    cin>>target;
+    cout<<f(arr,0,7,target,8);
+    return 0;
+}
+//time complexity is same as binary search
