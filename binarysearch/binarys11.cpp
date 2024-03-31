514d57db85328896160d4d959ee4219a9bbd587a
rishig1030 <guptarishi1030@gmail.com>
2024-03-31 14:22:46 +0530

    binary search 11 , find minimum of rotated sorted array with duplicate entries

--- binarysearch/binarys11.cpp
+++ binarysearch/binarys11.cpp
@@ -1,10 +1,38 @@
 // find minimum in rotated sorted array , with duplicate element 
 //do it yourself
+//find element in duplicate has already have a concept to tackle with duplicate element
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int main(){
-    
 
+int f(int arr[],int n){
+
+    int left = 0;
+    int right = n-1;
+    int mini=INT16_MAX;
+    while(left<=right){
+
+        int mid = (left+right)/2;
+        
+        mini= min(mini,arr[mid]);
+        if(arr[mid]>arr[left]){
+            mini=min(mini,arr[left]);
+            left=mid+1;
+        }
+        else {
+            mini=min(mini,arr[mid]);
+            right= mid-1;
+        }
+        
+        
+
+    }
+    return mini; 
+
+}
+
+int main(){
+    int arr[]={4,5,5,6,7,8,8,8,8,8,2,2,2,3,4,4,4};
+    cout<<f(arr,17);
     return 0;
 }
