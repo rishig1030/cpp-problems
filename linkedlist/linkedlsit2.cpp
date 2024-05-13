3fbf82c273417f208e72fa864774558ec3b7d9c0
rishig1030 <guptarishi1030@gmail.com>
2024-05-13 17:16:02 +0530

    linked list 2,deleting element from sll

--- linkedlist/linkedlsit2.cpp
+++ linkedlist/linkedlsit2.cpp
@@ -1,4 +1,4 @@
-//inserting element at the middle of linked list 
+//deleting an element from linked list 
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,23 +6,69 @@ class node{
     public:
     int data;
     node* next;
+    //constructor
     node(int data){
         this->next=NULL;
         this->data=data;
     }
 };
 
-//insert the element at the end of linkled list
-void insert2ll(node* &tail,int data){
-    node* node1 =new node(data);
+void deletenode(int position,node* &head,node* &tail){
     
+    //deleting starting node
+    if(position == 1){
+        node* temp = head;
+        head = head->next;
+        //memory free
+        temp->next=NULL;
+        delete temp; // deleting by using manual distructor
+    }
+
+    else{
+        node* previous = NULL;
+        node* current = head;
+        int count =1;
+        while(count<position){
+            previous = current;
+            current = current->next;
+            count++;
+        }
+        previous->next = current->next;
+        if(previous->next==NULL){
+            tail=previous;
+        }
+        current->next = NULL;
+        delete current;
+    }
 
 }
 
+void insertattail(node* &tail,int data){
+    node* temp= new node(data);
+    tail->next=temp;
+    tail=temp;
+}
+
+void print(node* &head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
 int main(){
-    node* node1 = new node(10);
-    node* head=node1;
-    node* tail =node1;
-    insert2ll(tail,12);
+    node* node1= new node(10);
+    node* tail= node1;
+    node* head= node1;
+    insertattail(tail,12);
+    insertattail(tail,22);
+    insertattail(tail,42);
+    insertattail(tail,21);
+    print(head);
+    
+    deletenode(4,head,tail);
+    print(head);
     return 0;
 }
