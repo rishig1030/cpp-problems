fbaf021fddc1710869a8de15ead80eefda044148
rishig1030 <guptarishi1030@gmail.com>
2024-05-16 10:44:44 +0530

    linked list 5b , reverse a linked list recursively

---
+++ linkedlist/linkedlist5b.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+
+class node{
+    public:
+    int data;
+    node* next;
+    node(int data){
+        this->data= data;
+        this->next= NULL;
+    }
+};
+//insert in ll at the tail 
+void insert2tail(node* &tail, int d){
+    node* temp = new node(d);
+    tail->next = temp;
+    tail = tail->next;
+}
+
+//traverse a dll
+void print(node* &head){
+    node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+//reverse a dll 
+void reverse(node* &head,node* current,node* previous){
+    if(current == NULL){
+        head=previous;
+        return ;
+    }
+    
+    reverse(head,current->next,current);
+    current->next = previous;
+
+}
+
+void  reversell(node* &head){
+    node* current = head;
+    node* previous= NULL;
+    reverse(head,current,previous);
+}
+
+int main(){
+    node* temp= new node(2);
+    node* head= temp;
+    node* tail= temp;
+    insert2tail(tail,5);
+    insert2tail(tail,22);
+    insert2tail(tail,21);
+    insert2tail(tail,62);
+    insert2tail(tail,4);
+    print(head);
+    reversell(head);
+    print(head);
+
+    return 0;
+}
