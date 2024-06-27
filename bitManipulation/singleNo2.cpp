4fc80f4265758b3d6a4942e4744c410bbea4b463
rishig1030 <guptarishi1030@gmail.com>
2024-06-27 11:59:04 +0530

    find the unique number in an array , 3rd approach

--- bitManipulation/singleNo2.cpp
+++ bitManipulation/singleNo2.cpp
@@ -1,6 +1,5 @@
 // find the unique number present in an array which only contain the number in triplet
-#include<iostream>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
 int findunique(vector<int> v){
@@ -19,6 +18,37 @@ int findunique(vector<int> v){
         } 
     }
     return n;
+    // time complexity will always be O(N*32) 
+    // space complexity will be O(1).
+
+}
+
+// another solution
+
+int findunique2(vector<int> v){
+    sort(v.begin(),v.end()); // sorting the vector will take NlogN 
+    for(int i=1;i<v.size();i+=3){
+        if(v[i] != v[i-1]) return v[i-1];
+    }
+    return v[v.size()-1];
+
+    // time complexity will be O(NlogN + N/3) here log N to be 32 , n should be pow(2^32),
+    // that is not gonna happen
+    // space complexity O(1);
+    // only problem for this solution is that it alter the given input
+}
+
+// 3rd solution
+// time complexity is O(N)
+// space complexity is O(1)
+int findunique3(vector<int> v){
+    int one = 0;
+    int two = 0;
+    for(int i=0;i<v.size();i++){
+        one = (one^v[i]) & ~(two);
+        two = (two^v[i]) & ~(one);
+    }
+    return one;
 }
 
 int main(){
@@ -29,6 +59,8 @@ int main(){
         int a;cin>>a;
         v.push_back(a);
     }
-    cout<<findunique(v);
+    cout<<findunique(v)<<endl;
+    cout<<findunique2(v)<<endl;
+    cout<<findunique3(v)<<endl;
     return 0;
 }
